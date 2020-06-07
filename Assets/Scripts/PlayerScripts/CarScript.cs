a36d1e324329ae4a2012c518fd5ed5178b4a70dd
adde9708 <adde.edvin@gmail.com>
2020-06-07 21:04:59 +0200

    Update CarScript.cs

--- Assets/Scripts/PlayerScripts/CarScript.cs
+++ Assets/Scripts/PlayerScripts/CarScript.cs
@@ -1,91 +1,63 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class CarScript : MonoBehaviour
-{
+public class CarScript : MonoBehaviour {
 
-    public float rospeed = 90.0f;
+	public float rospeed = 90.0f;
 	public float acceleration = 0.0f;
 	public float revspeed = 0.0f;
 
+	void Update () {
 
-    
+		if (Input.GetKey (KeyCode.W)) {
 
+			Debug.Log ("Key W Pressed");
+			transform.Translate (new Vector3 (0, 0, acceleration * Time.deltaTime));
+			if (acceleration < 100)
+				acceleration += 2;
+		} else
 
-	void Update() 
-	{   
-
-	if (Input.GetKey(KeyCode.W))
-        {
-
-            Debug.Log("Key W Pressed");
-            transform.Translate(new Vector3(0, 0, acceleration * Time.deltaTime));
-			if (acceleration < 100) 
-			acceleration +=2;
-		}
-
-		else
-			
 			Debug.Log ("Nothing Pressed");
-        transform.Translate(new Vector3(0, 0, acceleration * Time.deltaTime));
+		transform.Translate (new Vector3 (0, 0, acceleration * Time.deltaTime));
 
 		if (acceleration > 0)
 			acceleration -= 1;
 
-
-		if(Input.GetKey (KeyCode.S))
-		{
-			Debug.Log("Key S Pressed.");
-            transform.Translate(new Vector3(0, 0, revspeed * Time.deltaTime));
+		if (Input.GetKey (KeyCode.S)) {
+			Debug.Log ("Key S Pressed.");
+			transform.Translate (new Vector3 (0, 0, revspeed * Time.deltaTime));
 			if (revspeed > -20)
 				revspeed = -20;
-		}
-
-		else
+		} else
 
 			Debug.Log ("Not Reversing");
-        transform.Translate(new Vector3(0, 0, revspeed * Time.deltaTime));
+		transform.Translate (new Vector3 (0, 0, revspeed * Time.deltaTime));
 
 		if (revspeed < 0)
 			revspeed += 1;
-		
 
+		if (Input.GetKey (KeyCode.D)) {
+			Debug.Log ("Key D Pressed");
+			transform.Rotate (new Vector3 (0, rospeed * Time.deltaTime, 0));
+		} else if (Input.GetKey (KeyCode.A)) {
+			Debug.Log ("Key A Pressed.");
+			transform.Rotate (new Vector3 (0, -rospeed * Time.deltaTime, 0));
+		}
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("Key D Pressed");
-            transform.Rotate(new Vector3(0, rospeed * Time.deltaTime, 0));
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("Key A Pressed.");
-            transform.Rotate(new Vector3(0,-rospeed * Time.deltaTime, 0));
-        }
-       
-    }
+	}
 
-
-
-	void OnTriggerStay(Collider other)
-	{
-		if (Input.GetKey (KeyCode.W)) 
-		{
-            transform.Translate (new Vector3 (0, 0, -acceleration * Time.deltaTime));
+	void OnTriggerStay (Collider other) {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (new Vector3 (0, 0, -acceleration * Time.deltaTime));
 			if (acceleration > 5)
 				acceleration = 5;
-		} 
-		else if (Input.GetKeyUp (KeyCode.W)) 
-		{
+		} else if (Input.GetKeyUp (KeyCode.W)) {
 			acceleration = 0;
 		}
 	}
 
-	void OnTriggerExit(Collider other) 
-	{
-		acceleration =+1;
+	void OnTriggerExit (Collider other) {
+		acceleration = +1;
 	}
 
-
 }
-
-
