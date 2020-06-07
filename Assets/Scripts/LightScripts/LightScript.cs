40ca652d640b7f77815738664e0132acfc408066
adde9708 <adde.edvin@gmail.com>
2020-06-07 21:00:26 +0200

    Revert "formatting"
    This reverts commit 468849cbd9e1e5a630cb51c0ac72804218569e60.

--- Assets/Scripts/LightScripts/LightScript.cs
+++ Assets/Scripts/LightScripts/LightScript.cs
@@ -1,27 +1,31 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class LightScript : MonoBehaviour {
-     public float amplitude = 0.2F;
-     public Light lt;
-     // Use this for initialization
-     void Start () {
-          amplitude = 0.2F;
-          lt = GetComponent<Light> ();
+public class LightScript : MonoBehaviour 
+{
+	public float amplitude = 0.2F;  
+	public Light  lt;
+    // Use this for initialization
+	void Start () {		
+		amplitude = 0.2F;
+		lt = GetComponent <Light>();    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {              
+        lt.intensity = amplitude;
+
+        if (Input.GetKey(KeyCode.L))
+        {   
+             amplitude = 1F; 
+            
+        }  
+        if(Input.GetKey(KeyCode.O))
+        {  
+           
+             amplitude = 0.2F;
+        } 
+
      }
-
-     // Update is called once per frame
-     void Update () {
-          lt.intensity = amplitude;
-
-          if (Input.GetKey (KeyCode.L)) {
-               amplitude = 1F;
-
-          }
-          if (Input.GetKey (KeyCode.O)) {
-
-               amplitude = 0.2F;
-          }
-
-     }
-}
+} 
